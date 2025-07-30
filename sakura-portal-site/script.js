fd547eb0069e50a8c042d6e8501a31c7f3ede659
amber2713 <2713033233@qq.com>
2025-07-30 13:35:14 +0800

    Update script.js

---
+++ sakura-portal-site/script.js
@@ -0,0 +1,5 @@
+document.getElementById('video-trigger').addEventListener('click', function () {
+  const video = document.getElementById('sakura-video');
+  video.style.display = 'block';
+  video.play();
+});
